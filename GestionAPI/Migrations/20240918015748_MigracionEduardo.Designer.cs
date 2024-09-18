899e35cf16095c2f34213d3ea43fc88defac7b55
Eduardo Martinez Jr <adonay.chacon12@gmail.com>
2024-09-17 23:29:37 -0600

    Contenedor de Doocket

--- GestionAPI/Migrations/20240918015748_MigracionEduardo.Designer.cs
+++ GestionAPI/Migrations/20240918015748_MigracionEduardo.Designer.cs
@@ -4,6 +4,7 @@ using GestionAPI.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GestionAPI.Migrations
 {
     [DbContext(typeof(GestionDbContext))]
-    partial class GestionDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240918015748_MigracionEduardo")]
+    partial class MigracionEduardo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ GestionAPI/Migrations/20240918015748_MigracionEduardo.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestionAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigracionEduardo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
