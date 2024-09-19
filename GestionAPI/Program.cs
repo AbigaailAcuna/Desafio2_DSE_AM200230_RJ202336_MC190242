71c0af8f4637c4cc1f9d3d69abab76bcb05b5315
Abigail Acuña <acuaabigail@yahoo.com>
2024-09-19 09:35:05 -0600

    Agregando Docker

--- GestionAPI/Program.cs
+++ GestionAPI/Program.cs
@@ -4,38 +4,42 @@ using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
+// Configura el contexto de la base de datos
 builder.Services.AddDbContext<GestionDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-// Add services to the container.
-builder.Services.AddDbContext<GestionDbContext>(options => options.UseInMemoryDatabase("GestionInMemoryDb"));
-//Redis
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Configura Redis
 builder.Services.AddStackExchangeRedisOutputCache(opciones =>
 {
     opciones.Configuration = builder.Configuration.GetConnectionString("redis");
 });
-//Registrar IConnectionMultiplexer
+
+// Registrar IConnectionMultiplexer
 builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
 {
     var configuration = ConfigurationOptions.Parse(builder.Configuration.GetConnectionString("redis"), true);
     return ConnectionMultiplexer.Connect(configuration);
 });
+
 builder.Services.AddOutputCache();
 
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Configura Swagger/OpenAPI
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.RoutePrefix = string.Empty; 
+    });
 }
 
 app.UseHttpsRedirection();
